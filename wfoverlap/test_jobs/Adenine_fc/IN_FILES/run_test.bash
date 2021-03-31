95f62a6fdc53000033baf863e1f0b363a90ebe7a
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2021-03-31 11:51:28 +0100

    update tests

--- wfoverlap/test_jobs/Adenine_fc/IN_FILES/run_test.bash
+++ wfoverlap/test_jobs/Adenine_fc/IN_FILES/run_test.bash
@@ -8,5 +8,5 @@ $OVDIR/../bin/wfoverlap.x -f ciov_fc_direct.in -m 9  > ciov_low-mem_direct.out
 $OVDIR/../bin/wfoverlap.x -f ciov_fc.in -m 8  > ciov_very-low-mem.out
 
 export OMP_NUM_THREADS=2 # If there are more threads, then more memory would be needed!
-$OVDIR/../bin/wfoverlap.x -f dyson.in -m 4 > dyson.low-mem.out
+$OVDIR/../bin/wfoverlap.x -f dyson.in -m 5 > dyson.low-mem.out
 $OVDIR/../bin/wfoverlap.x -f ciov_fc.in -m 9  > ciov_very-low-mem2.out
